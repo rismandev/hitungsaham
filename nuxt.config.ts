6755db02dee9028e4fd05f4f3d8ca79a792233e0
Risman Abdilah <risman.abdilah@frisidea.com>
2025-07-29 14:37:59 +0700

    update base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
   app: {
+    baseURL: "/",
     head: {
       title:
         "HitungSaham - Kalkulator & Alat Bantu Perhitungan Saham Terlengkap",
